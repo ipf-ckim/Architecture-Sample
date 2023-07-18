f66ff4edbc39bedf204be94c0423f0ffcc8c6a9a
Chamsol Kim <ckim@iportfolio.co.kr>
2023-07-18 15:35:17 +0900

    Initial Commit

---
+++ ArchitectureSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArchitectureSample
+//
+//  Created by Chamsol Kim on 2023/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
